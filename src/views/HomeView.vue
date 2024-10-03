6da9aff4018bbc5a3503274a50b5ee00a5fd21f4
小堇 <huanlei@ky27.onexmail.com>
2024-10-03 23:21:21 +0800

    博客初步

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,19 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <h1>个人博客</h1>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
+  data () {
+    return {}
   }
 }
 </script>
+
+<style scoped>
+h1 {
+  text-align: center;
+}
+</style>
---
+++ src/views/PostView.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <div v-for="item in posts" :key="item.id">
+      <h2>{{ item.title }}</h2>
+      <p>{{ item.content }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      posts: [
+        { id: 1, title: '我的第一篇博客', content: '这是我第一篇博客内容...' },
+        {
+          id: 2,
+          title: 'Vue.js 学习心得',
+          content: '今天我学到了 Vue 的基础知识...'
+        },
+        {
+          id: 3,
+          title: '聊聊前端开发',
+          content: '前端开发是一个非常有趣的领域...'
+        }
+      ]
+    }
+  },
+  created () {}
+}
+</script>
+
+<style scoped>
+h2 {
+  text-align: center;
+}
+</style>
